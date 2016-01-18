0b700b3141f6cd1d57af227f96ec57e0cb280faf
robertwbrandt <bob@brandt.ie>
2016-01-18 11:51:52 +0000

    4

--- zarafa-backup.py
+++ zarafa-backup.py
@@ -2,7 +2,7 @@
 """
 Script used to backup Zarafa Mailboxes using brick-level-backup commands.
 """
-import argparse, os, subprocess, sys, datetime, fnmatch
+import argparse, os, subprocess, datetime
 import xml.etree.ElementTree as ElementTree
 
 args = {}
@@ -14,6 +14,7 @@ args['log'] = ''
 args['threads'] = 4
 
 zarafaAdmin = '/usr/sbin/zarafa-admin'
+zarafaBackup = '/usr/sbin/zarafa-backup'
 encoding = "utf-8"
 
 def command_line_args():
@@ -36,7 +37,7 @@ def command_line_args():
                     help='Display output type.')
   parser.add_argument('-l', '--location',
                     required=False,
-                    default=args['location'],                    
+                    default=args['location'],
                     type=str,
                     action='store')  
   parser.add_argument('--log',
@@ -45,6 +46,7 @@ def command_line_args():
                     action='store')
   parser.add_argument('-t', '--threads',
                     required=False,
+                    default=args['threads'],
                     type=int,
                     action='store')
   args.update(vars(parser.parse_args()))
@@ -66,5 +68,14 @@ if __name__ == "__main__":
   if err or rc:
     exit(err)
 
-  users = [ s.strip().split('\t')[0] for s in str(out).split('\n')[4:] if s ]
-  print users
+  for user in sorted([ str(s.strip().split('\t')[0]).lower() for s in str(out).split('\n')[4:] if s ]):
+    dateStr = str(datetime.datetime.now().strftime('%a %b %d %H:%M:%S %Y:')).ljust(26)
+
+    print dateStr + str('[zarafa-backup] [ notice]').rjust(36) + ' Starting backup of user ' + user
+    # p = subprocess.Popen([zarafaBackup, '-t', ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # out, err = p.communicate()
+    # rc = p.returncode
+
+
+#    /usr/sbin/zarafa-backup -t $_backupThreads -a -o "$_backupLocation" 2>&1 | tee "$_backupLog" 
+# Mon Jan 18 00:00:06 2016: [ECThreadPool|0x6a01c700] [ notice] Backup of user KENNEDYK with 1/2414 items in 17/17 folders, written 173 KB
