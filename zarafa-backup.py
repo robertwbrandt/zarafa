1020c560ea7409e7c1c9fa6ef656c255256f8246
robertwbrandt <bob@brandt.ie>
2016-01-18 11:10:26 +0000

    3

--- zarafa-backup.py
+++ zarafa-backup.py
@@ -49,9 +49,7 @@ def command_line_args():
   args.update(vars(parser.parse_args()))
 
   if not os.direxists(args['location']):
-    exit('The path specified (' + str(args['location'] + ') does not exist.')
-
-  print args
+    exit('The path specified (' + str(args['location']) + ') does not exist.')
   if not args['log']:
     args['log'] = os.path.join(args['location'], 'backup.log')
 
