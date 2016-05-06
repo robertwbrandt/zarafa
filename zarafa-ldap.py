0e7a4f96ea91137b667af04a97b25ea91880f567
Bob Brandt <bob@brandt.ie>
2016-05-06 12:02:53 +0100

    Work 2016-05-06 12:02:53

--- zarafa-ldap.py
+++ zarafa-ldap.py
@@ -120,10 +120,16 @@ def write_zarafa_cache():
     if zarafaLDAP.has_key('ldap_port'): ZarafaLDAPURL += ':' + zarafaLDAP['ldap_port']
   if ZarafaLDAPURL[-1] != "/": ZarafaLDAPURL += '/'
   ZarafaLDAPURL += "?" + zarafaLDAP.get('ldap_search_base','')
-  ZarafaLDAPURL += "?" + ",".join(sorted(zarafaAttrs))
+  
+  # ZarafaLDAPURL += "?" + ",".join(sorted(zarafaAttrs))
+  ZarafaLDAPURL += "?assistant,cn,co,company,department,displayname"
+
   ZarafaLDAPURL += "?sub"
+
   ZarafaLDAPURL += "?" + zarafaFilter
-  if zarafaLDAP.has_key('ldap_bind_user'): ZarafaLDAPURL += "?bindname=" + zarafaLDAP['ldap_bind_user'] + ",X-BINDPW=" + zarafaLDAP['ldap_bind_passwd']
+
+  # if zarafaLDAP.has_key('ldap_bind_user'): ZarafaLDAPURL += "?bindname=" + zarafaLDAP['ldap_bind_user'] + ",X-BINDPW=" + zarafaLDAP['ldap_bind_passwd']
+  print ZarafaLDAPURL
   results = brandt.LDAPSearch(ZarafaLDAPURL).results
 
 
