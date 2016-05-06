23cd8be2c2a8d7181e621f68f93a0c0dba1a6cdc
Bob Brandt <bob@brandt.ie>
2016-05-06 10:49:07 +0100

    Work 2016-05-06 10:49:07

---
+++ zarafa-ldap.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+"""
+Python utility to monitor when LDAP attributes change and issue --sync command to Zarafa
+"""
+import argparse, textwrap, fnmatch, datetime
+import xml.etree.cElementTree as ElementTree
+import subprocess
+
+# Import Brandt Common Utilities
+import sys, os
+sys.path.append( os.path.realpath( os.path.join( os.path.dirname(__file__), "/opt/brandt/common" ) ) )
+import brandt
+sys.path.pop()
+
+args = {}
+
+version = 0.3
+encoding = 'utf-8'
+
+zarafaFiles = {'server.cfg': '/etc/zarafa/server.cfg',
+               'ldap.propmap.cfg': '/etc/zarafa/ldap.propmap.cfg',
+               'ldap.cfg': '/etc/zarafa/ldap.active-directory.cfg'}
+
+
+class customUsageVersion(argparse.Action):
+  def __init__(self, option_strings, dest, **kwargs):
+    self.__version = str(kwargs.get('version', ''))
+    self.__prog = str(kwargs.get('prog', os.path.basename(__file__)))
+    self.__row = min(int(kwargs.get('max', 80)), brandt.getTerminalSize()[0])
+    self.__exit = int(kwargs.get('exit', 0))
+    super(customUsageVersion, self).__init__(option_strings, dest, nargs=0)
+  def __call__(self, parser, namespace, values, option_string=None):
+    # print('%r %r %r' % (namespace, values, option_string))
+    if self.__version:
+      print self.__prog + " " + self.__version
+      print "Copyright (C) 2013 Free Software Foundation, Inc."
+      print "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>."
+      version  = "This program is free software: you can redistribute it and/or modify "
+      version += "it under the terms of the GNU General Public License as published by "
+      version += "the Free Software Foundation, either version 3 of the License, or "
+      version += "(at your option) any later version."
+      print textwrap.fill(version, self.__row)
+      version  = "This program is distributed in the hope that it will be useful, "
+      version += "but WITHOUT ANY WARRANTY; without even the implied warranty of "
+      version += "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
+      version += "GNU General Public License for more details."
+      print textwrap.fill(version, self.__row)
+      print "\nWritten by Bob Brandt <projects@brandt.ie>."
+    else:
+      print "Usage: " + self.__prog + " [options]"
+      print "Python utility to monitor when LDAP attributes change and issue --sync command to Zarafa.\n"
+      print "Options:"
+      options = []
+      options.append(("-h, --help",              "Show this help message and exit"))
+      options.append(("-v, --version",           "Show program's version number and exit"))
+      length = max( [ len(option[0]) for option in options ] )
+      for option in options:
+        description = textwrap.wrap(option[1], (self.__row - length - 5))
+        print "  " + option[0].ljust(length) + "   " + description[0]
+      for n in range(1,len(description)): print " " * (length + 5) + description[n]
+    exit(self.__exit)
+def command_line_args():
+  global args, version
+  parser = argparse.ArgumentParser(add_help=False)
+  parser.add_argument('-v', '--version', action=customUsageVersion, version=version, max=80)
+  parser.add_argument('-h', '--help', action=customUsageVersion)
+  args.update(vars(parser.parse_args()))
+
+def read_zarafa_cache():
+  pass
+
+
+def write_zarafa_cache():
+  global zarafaFiles
+
+  f = open(zarafaFiles['server.cfg'], 'r')
+  out = f.read().split('\n')
+  f.close()
+  for line in out:
+    if str(line)[:18].lower() == "user_plugin_config":
+      line = line.split("=",1)
+      if len(line) == 2: 
+        zarafaFiles['ldap.cfg'] = str(line[1]).strip()
+        print zarafaFiles['ldap.cfg']
+        break
+
+  print "Reading", zarafaFiles['ldap.cfg']
+  # f = open(zarafaFiles['server.cfg'], 'r')
+  # out = f.read().split('\n')
+  # f.close()
+  # for line in out:
+  #   if str(line)[:18].lower() == "user_plugin_config":
+  #     line = line.split("=",1)
+  #     if len(line) == 2: 
+  #       zarafaFiles['ldap.cfg'] = str(line[1]).strip()
+  #       break
+
+
+
+# Get LDAP file
+# sed -n "s|user_plugin_config\W*=||p" /etc/zarafa/server.cfg 
+
+# Get LDAP properties
+# cat /etc/zarafa/ldap.active-directory.cfg
+
+# Attributes
+# sed -ne 's|.*=\W*||p' /etc/zarafa/ldap.propmap.cfg   
+
+
+
+
+
+
+# Start program
+if __name__ == "__main__":
+  command_line_args()  
+
+  write_zarafa_cache()
+
+
+
