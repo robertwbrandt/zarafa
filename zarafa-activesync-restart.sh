7535f070a78bc8ac95ca7afddbbc589a93571dd7
Bob Brandt <bob@brandt.ie>
2016-09-15 21:54:58 +0100

    home 2016-09-15 21:54:58

--- zarafa-activesync-restart.sh
+++ zarafa-activesync-restart.sh
@@ -3,11 +3,17 @@
 #     Script to restart Apache for Activesync
 #     Bob Brandt <projects@brandt.ie>
 # 
-if nc -v -z activesync.opw.ie 443
+#if nc -v -z activesync.opw.ie 443
+
+
+
+
+#if nc -v -z activesync.opw.ie 443
+if wget --timeout=10 --no-proxy --no-check-certificate -O /dev/null https://activesync.opw.ie/Microsoft-Server-ActiveSync
 then
 	logger -st 'activesync' 'Connection to activesync.opw.ie 443 port [tcp/https] succeeded!'
 else
-	logger -st 'activesync' 'connect to activesync.opw.ie port 443 (tcp) failed: Connection refused. Restarting Apache2.'
+	logger -st 'activesync' 'Connection to activesync.opw.ie 443 port [tcp/https] failed: Connection refused. Unable to establish SSL connection.'
 	/etc/init.d/apache2 restart
 fi
 exit $?
