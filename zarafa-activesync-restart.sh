a8ae4a289010fd651fb3fc2b704126aa8d93ac8f
Bob Brandt <bob@brandt.ie>
2016-09-15 13:43:20 +0100

    Work 2016-09-15 13:43:20

--- zarafa-activesync-restart.sh
+++ zarafa-activesync-restart.sh
@@ -3,9 +3,11 @@
 #     Script to restart Apache for Activesync
 #     Bob Brandt <projects@brandt.ie>
 # 
-if ! nc -v -z activesync.opw.ie 443
+if nc -v -z activesync.opw.ie 443
 then
-	logger -st 'activesync' 'Activesync(apache2) is not responding on port 443. Restarting Apache2.'
-	echo /etc/init.d/apache2 restart
+	logger -st 'activesync' 'Connection to activesync.opw.ie 443 port [tcp/https] succeeded!'
+else
+	logger -st 'activesync' 'connect to activesync.opw.ie port 443 (tcp) failed: Connection refused. Restarting Apache2.'
+	/etc/init.d/apache2 restart
 fi
 exit $?
