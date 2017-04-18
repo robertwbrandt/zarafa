c74a1961b576bf63cd688167bb98e81db7c0a977
Bob Brandt <bob@brandt.ie>
2017-04-18 16:04:54 +0100

    Work 2017-04-18 16:04:54

--- utilities/read_webapp-signatures.py
+++ utilities/read_webapp-signatures.py
@@ -70,7 +70,8 @@ if __name__ == '__main__':
             data = json.loads(str(raw_data)).get('settings',{}).get('zarafa',{}).get('v1',{}).get('contexts',{}).get('mail',{}).get('signatures',{})
             output = {}
             output['signatures'] = {}
-            print data.get('all',{}).keys()
+            for key in data.get('all',{}).keys():
+                print keys
             output['new_message'] = data.get('new_message','')
             output['replyforward_message'] = data.get('replyforward_message','')
 
