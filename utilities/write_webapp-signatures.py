9239634a8fec19f2f36019e3db8f26ce0431b717
Bob Brandt <bob@brandt.ie>
2017-04-20 21:02:10 +0100

    home 2017-04-20 21:02:10

--- utilities/write_webapp-signatures.py
+++ utilities/write_webapp-signatures.py
@@ -184,7 +184,7 @@ if __name__ == "__main__":
                     f = open(args['file'], 'r')
                     args['file'] = f.read()
                     f.close()
-                args['file'] = "".join(args['file']).split()
+                #args['file'] = "".join(args['file']).split()
 
 
                 print args['file']
