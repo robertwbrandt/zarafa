d6ffde2477e35ed4d624c8486269af84fab8f5b7
Bob Brandt <bob@brandt.ie>
2017-04-21 13:48:25 +0100

    Work 2017-04-21 13:48:24

--- utilities/write_webapp-signatures.py
+++ utilities/write_webapp-signatures.py
@@ -195,11 +195,11 @@ if __name__ == "__main__":
 					# Get Milliseconds since Epoch
 					epoch = int(time.time() * 1000)
 					data['settings']['zarafa']['v1']['contexts']['mail']['signatures']['all'][str(epoch)] = {'content': str(args['file']), 'isHTML': not bool(args['text']), 'name': str(args['name'])}
-					
+
 					if args['new']:
 						data['settings']['zarafa']['v1']['contexts']['mail']['signatures']['new_message'] = epoch
 					if args['reply']:
 						data['settings']['zarafa']['v1']['contexts']['mail']['signatures']['replyforward_message'] = epoch
 
-					#pprint.pprint( data['settings']['zarafa']['v1']['contexts']['mail']['signatures'] )
-					write_settings(args['user'], data):
+					pprint.pprint( data['settings']['zarafa']['v1']['contexts']['mail']['signatures'] )
+					#write_settings(args['user'], data)
